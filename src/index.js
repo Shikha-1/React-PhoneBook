0841d3ef7fa24b706c243d570d4966c4edc83655
Shikha <shikhakumarisingh4@gmail.com>
2021-06-29 10:10:49 +0530

    replaced PhoneBook components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ToDoApp from './ToDoList/ToDoApp'
+import App from './PhoneBook/components/App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ToDoApp/>
+    <App/>
   </React.StrictMode>,
   document.getElementById("root")
 );
